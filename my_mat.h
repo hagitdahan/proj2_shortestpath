cdd1a94fb9ce1e190e77c23a9cd5414acbec9e30
hagit dahan <hagit.dahan@msmail.ariel.ac.il>
2024-02-04 17:24:34 +0200

    upload question1

--- my_mat.h
+++ my_mat.h
@@ -1,8 +1,8 @@
 #ifndef MY_MAT_H_
 #define N 3
-void buildMat(int mat[][N]);
+void buildMat(int mat[N][N]);
 int min(int x,int y);
-void calculateMatrix(int mat[][N]);
-int thereIsPath(int,int,int mat[][N]);
-int shorsetPath(int i,int j, int mat[][N]);
+void calculateMatrix(int mat[N][N]);
+int thereIsPath(int,int,int mat[N][N]);
+int shorsetPath(int i,int j, int mat[N][N]);
 #endif
